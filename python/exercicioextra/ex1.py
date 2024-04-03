4e2fa1794c85ea177cafe2d5a5ced9611f69fd89
LarissaCoder <larissasilva2709@gmail.com>
2024-04-03 00:53:49 -0300

    Excluído arquivo de teste

--- python/exercicioextra/ex1.py
+++ python/exercicioextra/ex1.py
@@ -22,14 +22,14 @@ sobrenome = input('Qual o seu sobrenome?')
 print("Seu sobrenome é",sobrenome)
 
 '''Desafio 1
-    Crie um script que leia o nome de uma pessoae mostre uma mensagem de boas vindas de acordo com o valor digitado.
+Crie um script que leia o nome de uma pessoae mostre uma mensagem de boas vindas de acordo com o valor digitado.
 '''
 
 nome1 = input('Qual é o seu nome?')
 print('Seja bem-vinda', nome1, 'prazer em te conhecer')
 
 '''Desafio 2
-    Crie um script python que leia o dia, mês e ano da nascimento de uma pessoa e mostre uma mensagem com data formatada.
+Crie um script python que leia o dia, mês e ano da nascimento de uma pessoa e mostre uma mensagem com data formatada.
 '''
 
 dia = input('Qual o dia do seu nascimento? ')
@@ -39,7 +39,7 @@ ano = input('Qual o ano do seu nascimento? ')
 print('Você nasceu no dia',dia,'do mês de',mes,'e ano de', ano )
 
 '''Desafio 3
-    Crie um script python que leia dois números e tente mostrar a soma entre eles
+Crie um script python que leia dois números e tente mostrar a soma entre eles
 '''
 
 numero1 = int(input('Insira o primeiro número: '))
--- python/exercicioextra/test.py
+++
@@ -1,6 +0,0 @@
-numero1 = int(input('Insira o primeiro número: '))
-numero2 = int(input('Insira o segundo número: '))
-
-soma = numero1 + numero2
-
-print('A soma entre os dois número é de',soma)
